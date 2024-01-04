54512df720087d17d0a0e93b8c580cdca9e88f8c
Deykina_Ksenia <khomyasko@gmail.com>
2024-01-04 20:55:19 +0300

    Tests

---
+++ test_items.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+import time
+
+link=r'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_open_page(browser):
+    browser.get(link)
+    time.sleep(30)
+
+    assert browser.find_elements(By.ID,"add_to_basket_form"), "button is not found"
+
+
